a0e6050ccf6940ea91fa8e5046cf8ec53d22d0a0
AtenaHatta <atn041102@gmail.com>
2025-05-06 11:08:46 -0700

    fix：add cors to middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,25 @@
-export default [
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::cors',
+//   'strapi::security',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public'
+// ];
+
+module.exports = [
   'strapi::logger',
   'strapi::errors',
+  'strapi::cors',
   'strapi::security',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public'
+  'strapi::public',
 ];
